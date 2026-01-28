8476a4cedc4c9589a316e2dc86689fa479dd7064
Tofulati <almtho2003@gmail.com>
2026-01-28 12:24:10 -0800

    changes to html adding echo and updating syntax

--- hw2/part2/php/hello-html-php.php
+++ hw2/part2/php/hello-html-php.php
@@ -2,7 +2,7 @@
     $team = "HAJ135";
     $lang = "PHP";
     $datetime = date("m-d-Y H:i:s");
-    $ip = $_SERVER('REMOTE_ADDR');
+    $ip = $_SERVER['REMOTE_ADDR'];
 ?>
 
 <!DOCTYPE html>
@@ -11,10 +11,10 @@
         <title>Hello HTML PHP</title>
     </head>
     <body>
-        <h1>Hello from <? $team ?></h1>
+        <h1>Hello from <? echo $team ?></h1>
         <p>Welcome to the "Hello HTML PHP" page - Albert, Hajin, Joey</p>
-        <p>Coding language: <? $lang ?></p>
-        <p>Generated at: <? $datetime ?></p>
-        <p>Your IP address: <? $ip ?></p>
+        <p>Coding language: <? echo $lang ?></p>
+        <p>Generated at: <? echo $datetime ?></p>
+        <p>Your IP address: <? echo $ip ?></p>
     </body>
 </html>
