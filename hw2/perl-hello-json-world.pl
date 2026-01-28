6594b1257972c92a42aa2ab838f54e2cfae7a25c
Tofulati <almtho2003@gmail.com>
2026-01-27 22:21:16 -0800

    update server with hw2 content and part1

--- hw2/perl-hello-json-world.pl
+++ hw2/perl-hello-json-world.pl
@@ -1,6 +1,8 @@
 #!/usr/bin/perl
 # In Perl, you must first install the JSON package from CPAN (the Perl equivalent to npm)
 use JSON;
+use warnings;
+use strict;
 
 print "Cache-Control: no-cache\n";
 print "Content-type: application/json\n\n";
@@ -8,7 +10,13 @@ print "Content-type: application/json\n\n";
 $date = localtime();
 $address = $ENV{REMOTE_ADDR};
 
-my %message = ('title' => 'Hello, Everyone!', 'heading' => 'Hello, Perl!', 'message' => 'This page was generated with our love for the Perl programming language - Albert, Hajin, and Joey', 'time' => $date, 'IP' => $address);
+my %message = (
+    'title' => 'Hello, Everyone!',
+    'heading' => 'Hello, Perl!',
+    'message' => 'This page was generated with our love for the Perl programming language - Albert, Hajin, and Joey', 
+    'time' => $date, 
+    'IP' => $address
+);
 
 my $json = encode_json \%message;
 print "$json\n";
