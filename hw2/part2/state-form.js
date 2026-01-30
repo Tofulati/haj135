6ec84378fb9889b93636c7c77e8d3e26a39e0666
Hajin Park <hajin1819@gmail.com>
2026-01-30 12:01:27 -0800

    Init state forms; refactor echo form

---
+++ hw2/part2/state-form.js
@@ -0,0 +1,41 @@
+document.getElementById("echoForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const endpoint = document.getElementById("endpoint").value;
+    const method = document.getElementById("httpMethod").value;
+    const encoding = document.getElementById("encodingMethod").value;
+    const responseBox = document.getElementById("response");
+
+    const data = {
+        name: document.getElementById("name").value,
+        message: document.getElementById("message").value
+    }
+
+    let url = endpoint;
+    let options = {
+        method: method,
+        headers: {}
+    };
+
+    if (method === "GET") {
+        url += "?" + new URLSearchParams(data).toString();
+    } else {
+        if (encoding === "application/json") {
+            options.headers["Content-Type"] = "application/json";
+            options.body = JSON.stringify(data);
+        } else {
+            options.headers["Content-Type"] = "application/x-www-form-urlencoded";
+            options.body = new URLSearchParams(data).toString();
+        }
+    }
+
+    fetch(url, options)
+        .then(res => res.text())
+        .then(text => {
+            responseBox.textContent = text;
+        })
+        .catch(err => {
+            responseBox.textContent = "Request Failed";
+            console.log(err);
+        });
+});
