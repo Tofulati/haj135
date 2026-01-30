edae8430c8b3073b24b940fb82a34a0e6f5a415b
Hajin Park <hajin1819@gmail.com>
2026-01-30 12:52:48 -0800

    Add cookie check in state display page

---
+++ hw2/part2/state-display.js
@@ -0,0 +1,34 @@
+function getCookie(name) {
+    // The regular expression looks for the cookie name with optional leading 
+    // characters and a semicolon or end of string as a boundary.
+    const match = document.cookie.match(new RegExp('(^|;\\s*)' + name + '=([^;]*)'));
+    return match ? decodeURIComponent(match[2]) : null;
+}
+
+const display = document.getElementById("cookie-display");
+const clear_cookies_btn = document.getElementById("clear-cookies-btn")
+const username = getCookie("name");
+const message = getCookie("message");
+
+clear_cookies_btn.disabled = true;
+
+// Display cookies if set
+if (username && message) {
+    display.innerHTML = `
+    <h3>Name</h3>
+    <p>${username}</p>
+    <h3>Message</h3>
+    <p>${message}</p>
+    `;
+
+    clear_cookies_btn.disabled = false;
+    clear_cookies_btn.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+    });
+} else {
+    display.innerHTML = `
+    <h3>Cookies Not Found</h3>
+    <p>Set cookies at <a href="/hw2/part2/state-form.html">State Form</a></p>
+    `;
+}
